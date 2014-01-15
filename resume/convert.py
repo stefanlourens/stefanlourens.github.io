5f76c99aed1a0f6aff554e8e9e14ba26988f4a59
Stefan Lourens <slourens@tagworldwide.com>
2014-01-15 11:56:24 +0200

    Cleanup

--- resume/convert.py
+++ resume/convert.py
@@ -2,6 +2,8 @@
 
 from misaka import Markdown, HtmlRenderer
 from subprocess import call
+import time
+import sys
 
 renderer = Markdown(HtmlRenderer())
 
@@ -10,34 +12,42 @@ light_css = """<link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.
 
 template = """
 <!doctype html>
-<head>
-    <meta charset='utf - 8'>
-    <title>Stefan Lourens - Resume</title>
-    {0}
-</head>
-<body>
-<div style="width: 780px; margin:0 auto;">
-{1}
-</div>
-</body>
+<html>
+	<head>
+	    <meta charset='utf - 8'>
+	    <title>Stefan Lourens - Resume</title>
+	    {0}
+	</head>
+	<body>
+	<div style="width: 780px; margin:0 auto;">
+		{1}
+	</div>
+	</body>
 </html>
 """
 
-print "Reading markdown"
-file = open('resume.md', 'r')
-html = renderer.render(file.read())
+def main():
+	print "Reading markdown"
+	file = open('resume.md', 'r')
+	html = renderer.render(file.read())
 
-print "Converting to HTML"
-file = open('resume.html', 'w')
-file.write(template.format(light_css, html))
-file.flush()
-file.close()
+	print "Converting to HTML"
+	file = open('resume.html', 'w')
+	file.write(template.format(light_css, html))
+	file.flush()
+	file.close()
 
-print "Converting to HTML"
-file = open('resume_dark.html', 'w')
-file.write(template.format(dark_css, html))
-file.flush()
-file.close()
+	print "Converting to HTML"
+	file = open('resume_dark.html', 'w')
+	file.write(template.format(dark_css, html))
+	file.flush()
+	file.close()
 
-print "Converting to PDF"
-call(["wkhtmltopdf", "resume.html", "resume.pdf"])
+	print "Converting to PDF"
+	# wkhtmltopdf-0.11.0_rc1 does not exit properly
+	call(["wkhtmltopdf", "resume.html", "resume.pdf"])
+	
+
+if __name__ == "__main__":
+    main()
+    
